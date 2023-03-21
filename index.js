aa55bf42290c4ff1bff7bdd2584c1bb0966ef19a
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2023-03-21 01:57:20 -0300

    chat concluido

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const socketIo = require('socket.io');
+
+app.use('/grupo1', express.static(path.join(__dirname, 'public')));
+app.use('/grupo2', express.static(path.join(__dirname, 'public')));
+
+const server = app.listen(3000, () => {
+  console.log('Running');
+});
+
+const messages = { grupo1: [], grupo2: [] };
+
+const io = socketIo(server);
+
+function createMessageGroups(socket, group, ioGroup) {
+  console.log('new connection');
+  console.log(group)
+  socket.emit('update_messages', messages[group]);
+
+  socket.on('new_message', (data) => {
+    messages[group].push(data);
+    
+    ioGroup.emit('update_messages', messages[group]);
+  });
+}
+
+const grupo1 = io.of('/grupo1').on('connection', (socket) => {
+  // of permite criar um grupo
+  createMessageGroups(socket, 'grupo1', grupo1);
+});
+
+const grupo2 = io.of('/grupo2').on('connection', (socket) => {
+  // of permite criar um grupo
+  createMessageGroups(socket, 'grupo2', grupo2);
+});
+
+// io.on('connection', (socket) => {
+//   console.log('new connection');
+//   socket.emit('update_messages', messages.grupo1);
+
+//   socket.on('new_message', (data) => {
+//     messages.push(data);
+
+//     io.emit('update_messages', messages);
+//   });
+// });
